e7a60f0299f3fb9bd31a5644565edd120ad8081f
Kostyantyn Smozhevskykh <lee.ross2005@gmail.com>
2025-08-26 15:48:02 +0300

    impl few handlers for exceptions

--- src/main/java/org/example/springapie/exception/GlobalHandlerException.java
+++ src/main/java/org/example/springapie/exception/GlobalHandlerException.java
@@ -2,9 +2,33 @@ package org.example.springapie.exception;
 
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 @Slf4j
 public class GlobalHandlerException {
+
+    @ExceptionHandler(Exception.class)
+    public String handleGenericException(Exception ex, Model model) {
+        log.error("Unexpected error", ex);
+        model.addAttribute("errorMessage", "Unexpected error: " + ex.getMessage());
+        return "error/500";
+    }
+
+    @ExceptionHandler(NullEntityReferenceException.class)
+    public String handleNullPointerException(Exception ex, Model model) {
+        log.error("Null Entity Reference error", ex);
+        model.addAttribute("errorMessage", "Unexpected error: " + ex.getMessage());
+        return "error/404";
+    }
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public String handleEntityNotFoundException(Exception ex, Model model) {
+        log.error("Entity not found error", ex);
+        model.addAttribute("errorMessage", "Unexpected error: " + ex.getMessage());
+        return "error/404";
+    }
+
 }
